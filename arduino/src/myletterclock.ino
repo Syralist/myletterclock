ab91935f8e9d0f5fd2e64a3e65274978dbc2ccae
Thomas Helmke <thomas.helmke@gmx.de>
2015-03-28 21:04:06 +0100

    first test with LEDs

--- arduino/src/myletterclock.ino
+++ arduino/src/myletterclock.ino
@@ -1,7 +1,23 @@
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include <Wire.h>
 #include "RTClib.h"
+#include "Adafruit_NeoPixel.h"
 RTC_DS1307 rtc;
+//define the data pin
+#define PIN 10
+//define number of pixels
+#define NUMPIXEL 144
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXEL, PIN, NEO_GRB | NEO_KHZ800);
+
+unsigned int i = 0;
+
 void setup () {
     Serial.begin(57600);
 #ifdef AVR
@@ -18,6 +34,8 @@ void setup () {
         // January 21, 2014 at 3am you would call:
         // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
     }
+    strip.begin();                  // Initialize NeoPixel object
+    strip.show();                   // Initialize all pixels to '}ff'
 }
 void loop () {
     DateTime now = rtc.now();
@@ -54,5 +72,39 @@ void loop () {
     Serial.print(future.second(), DEC);
     Serial.println();
     Serial.println();
+    uint32_t color = 0;
+    color = strip.Color(0,0,0);
+    for(int j = 0; j < NUMPIXEL; j++)
+    {
+        strip.setPixelColor(j,color);
+    }
+    color = strip.Color(50,50,50);
+    strip.setPixelColor(i++,color);
+    strip.setPixelColor(6,color);
+    strip.setPixelColor(7,color);
+    strip.setPixelColor(8,color);
+    strip.setPixelColor(9,color);
+    strip.setPixelColor(10,color);
+    strip.setPixelColor(11,color);
+    strip.setPixelColor(31,color);
+    strip.setPixelColor(32,color);
+    strip.setPixelColor(33,color);
+    strip.setPixelColor(34,color);
+    strip.setPixelColor(77,color);
+    strip.setPixelColor(78,color);
+    strip.setPixelColor(79,color);
+    strip.setPixelColor(97,color);
+    strip.setPixelColor(98,color);
+    strip.setPixelColor(99,color);
+    strip.setPixelColor(100,color);
+    strip.setPixelColor(101,color);
+    strip.setPixelColor(102,color);
+    strip.setPixelColor(103,color);
+    strip.setPixelColor(132,color);
+    strip.setPixelColor(133,color);
+    strip.setPixelColor(138,color);
+    strip.setPixelColor(139,color);
+    strip.setPixelColor(140,color);
+    strip.show();
     delay(3000);
 }
