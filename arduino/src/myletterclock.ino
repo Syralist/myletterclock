141f4a20c77207567d1d6b1c8a44d2e49ccd7a65
Thomas Helmke <thomas.helmke@gmx.de>
2015-03-30 17:23:41 +0200

    replaced RTClib with DS1307RTC

--- arduino/src/myletterclock.ino
+++ arduino/src/myletterclock.ino
@@ -1,10 +1,15 @@
 // Date and time functions using a DS1307 RTC connected via I2C and Wire lib
 #include <Wire.h>
-#include "RTClib.h"
+#include <DS1307RTC.h>
+#include <Time.h>
 #include "Adafruit_NeoPixel.h"
 #include "TimerOne.h"
+#include "DCF77.h"
 
-RTC_DS1307 rtc;
+#define DCF_PIN 2                // Connection pin to DCF 77 device
+#define DCF_INTERRUPT 0          // Interrupt number associated with pin
+DCF77 DCF = DCF77(DCF_PIN,DCF_INTERRUPT, true);
+
 //define the data pin
 #define PIN 10
 //define number of pixels
@@ -27,30 +32,15 @@ Color cRed = 0;
 Color cGreen = 0;
 Color cBlue = 0;
 
-/* void lightHalb(Color color); */
-
 void setup ()
 {
     Timer1.initialize(1000000);       // Initialize timer to 1s
-    Timer1.attachInterrupt(checktime);  // Attach funciton checktime to timer interupt   
+    Timer1.attachInterrupt(checktime);  // Attach funciton checktime to timer interupt
     Serial.begin(57600);
-#ifdef AVR
-    Wire.begin();
-#else
-    Wire1.begin(); // Shield I2C pins connect to alt I2C bus on Arduino Due
-#endif
-    rtc.begin();
-    if (! rtc.isrunning())
-    {
-        Serial.println("RTC is NOT running!");
-        // following line sets the RTC to the date & time this sketch was compiled
-        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-        // This line sets the RTC with an explicit date & time, for example to set
-        // January 21, 2014 at 3am you would call:
-        // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
-    }
     strip.begin();                  // Initialize NeoPixel object
     strip.show();                   // Initialize all pixels to '}ff'
+
+    DCF.Start();
 
     // define global colors
     cBlack = strip.Color(0,0,0);
@@ -62,84 +52,16 @@ void setup ()
 
 void loop ()
 {
-    /* DateTime now = rtc.now(); */
-    /* Serial.print(now.year(), DEC); */
-    /* Serial.print('/'); */
-    /* Serial.print(now.month(), DEC); */
-    /* Serial.print('/'); */
-    /* Serial.print(now.day(), DEC); */
-    /* Serial.print(' '); */
-    /* Serial.print(now.hour(), DEC); */
-    /* Serial.print(':'); */
-    /* Serial.print(now.minute(), DEC); */
-    /* Serial.print(':'); */
-    /* Serial.print(now.second(), DEC); */
-    /* Serial.println(); */
-    /* Serial.print(" since midnight 1/1/1970 = "); */
-    /* Serial.print(now.unixtime()); */
-    /* Serial.print("s = "); */
-    /* Serial.print(now.unixtime() / 86400L); */
-    /* Serial.println("d"); */
-    /* // calculate a date which is 7 days and 30 seconds into the future */
-    /* DateTime future (now.unixtime() + 7 * 86400L + 30); */
-    /* Serial.print(" now + 7d + 30s: "); */
-    /* Serial.print(future.year(), DEC); */
-    /* Serial.print('/'); */
-    /* Serial.print(future.month(), DEC); */
-    /* Serial.print('/'); */
-    /* Serial.print(future.day(), DEC); */
-    /* Serial.print(' '); */
-    /* Serial.print(future.hour(), DEC); */
-    /* Serial.print(':'); */
-    /* Serial.print(future.minute(), DEC); */
-    /* Serial.print(':'); */
-    /* Serial.print(future.second(), DEC); */
-    /* Serial.println(); */
-    /* Serial.println(); */
-    /* uint32_t color = 0; */
-    /* color = strip.Color(0,0,0); */
-    /* for(int j = 0; j < NUMPIXEL; j++) */
-    /* { */
-    /*     strip.setPixelColor(j,color); */
-    /* } */
-    /* color = strip.Color(50,50,50); */
-    /* strip.setPixelColor(i++,color); */
-    /* strip.setPixelColor(6,color); */
-    /* strip.setPixelColor(7,color); */
-    /* strip.setPixelColor(8,color); */
-    /* strip.setPixelColor(9,color); */
-    /* strip.setPixelColor(10,color); */
-    /* strip.setPixelColor(11,color); */
-    /* strip.setPixelColor(31,color); */
-    /* strip.setPixelColor(32,color); */
-    /* strip.setPixelColor(33,color); */
-    /* strip.setPixelColor(34,color); */
-    /* strip.setPixelColor(77,color); */
-    /* strip.setPixelColor(78,color); */
-    /* strip.setPixelColor(79,color); */
-    /* strip.setPixelColor(97,color); */
-    /* strip.setPixelColor(98,color); */
-    /* strip.setPixelColor(99,color); */
-    /* strip.setPixelColor(100,color); */
-    /* strip.setPixelColor(101,color); */
-    /* strip.setPixelColor(102,color); */
-    /* strip.setPixelColor(103,color); */
-    /* strip.setPixelColor(132,color); */
-    /* strip.setPixelColor(133,color); */
-    /* strip.setPixelColor(138,color); */
-    /* strip.setPixelColor(139,color); */
-    /* strip.setPixelColor(140,color); */
-    /* strip.show(); */
-    /* delay(3000); */
 }
 
 void checktime()
 {
-    DateTime now = rtc.now();
-    int hour = now.hour();
-    int minute = now.minute();
+    tmElements_t tm;
+    RTC.read(tm);
+    int hour = tm.Hour;
+    int minute = tm.Minute;
     int minutemod5 = minute % 5;
-    int second = now.second();
+    int second = tm.Second;
     Color cBackground = cBlack;
     Color cForeground = cWhite;
     for(int j = 0; j < NUMPIXEL; j++)
@@ -167,34 +89,34 @@ void checktime()
     {
         lightGerade(cForeground);
     }
-    if((minute >= 3 && minute <= 7) || 
-            (minute >= 23 && minute <= 27) || 
+    if((minute >= 3 && minute <= 7) ||
+            (minute >= 23 && minute <= 27) ||
             (minute >= 33 && minute <= 37) ||
             (minute >= 53 && minute <= 57))
     {
         lightFuenf1(cForeground);
     }
-    if((minute >= 13 && minute <= 17) || 
+    if((minute >= 13 && minute <= 17) ||
             (minute >= 43 && minute <= 47))
     {
         lightViertel(cForeground);
     }
-    if((minute >= 8 && minute <= 12) || 
+    if((minute >= 8 && minute <= 12) ||
             (minute >= 48 && minute <= 52))
     {
         lightZehn1(cForeground);
     }
-    if((minute >= 18 && minute <= 22) || 
+    if((minute >= 18 && minute <= 22) ||
             (minute >= 38 && minute <= 42))
     {
         lightZwanzig(cForeground);
     }
-    if((minute >= 3 && minute <= 22) || 
+    if((minute >= 3 && minute <= 22) ||
             (minute >= 33 && minute <= 37))
     {
         lightNach1(cForeground);
     }
-    if((minute >= 23 && minute <= 27) || 
+    if((minute >= 23 && minute <= 27) ||
             (minute >= 38 && minute <= 57))
     {
         lightVor1(cForeground);
@@ -203,62 +125,62 @@ void checktime()
     {
         lightHalb(cForeground);
     }
-    if(((hour ==  1 || hour == 13) && minute <= 22) || 
+    if(((hour ==  1 || hour == 13) && minute <= 22) ||
             ((hour ==  0 || hour == 12) && minute >= 23))
     {
         lightEins(cForeground);
     }
-    if(((hour ==  2 || hour == 14) && minute <= 22) || 
+    if(((hour ==  2 || hour == 14) && minute <= 22) ||
             ((hour ==  1 || hour == 13) && minute >= 23))
     {
         lightZwei(cForeground);
     }
-    if(((hour ==  3 || hour == 15) && minute <= 22) || 
+    if(((hour ==  3 || hour == 15) && minute <= 22) ||
             ((hour ==  2 || hour == 14) && minute >= 23))
     {
         lightDrei(cForeground);
     }
-    if(((hour ==  4 || hour == 16) && minute <= 22) || 
+    if(((hour ==  4 || hour == 16) && minute <= 22) ||
             ((hour ==  3 || hour == 15) && minute >= 23))
     {
         lightVier(cForeground);
     }
-    if(((hour ==  5 || hour == 17) && minute <= 22) || 
+    if(((hour ==  5 || hour == 17) && minute <= 22) ||
             ((hour ==  4 || hour == 16) && minute >= 23))
     {
         lightFuenf2(cForeground);
     }
-    if(((hour ==  6 || hour == 18) && minute <= 22) || 
+    if(((hour ==  6 || hour == 18) && minute <= 22) ||
             ((hour ==  5 || hour == 17) && minute >= 23))
     {
         lightSechs(cForeground);
     }
-    if(((hour ==  7 || hour == 19) && minute <= 22) || 
+    if(((hour ==  7 || hour == 19) && minute <= 22) ||
             ((hour ==  6 || hour == 18) && minute >= 23))
     {
         lightSieben(cForeground);
     }
-    if(((hour ==  8 || hour == 20) && minute <= 22) || 
+    if(((hour ==  8 || hour == 20) && minute <= 22) ||
             ((hour ==  7 || hour == 19) && minute >= 23))
     {
         lightAcht(cForeground);
     }
-    if(((hour ==  9 || hour == 21) && minute <= 22) || 
+    if(((hour ==  9 || hour == 21) && minute <= 22) ||
             ((hour ==  8 || hour == 20) && minute >= 23))
     {
         lightNeun(cForeground);
     }
-    if(((hour ==  10 || hour == 22) && minute <= 22) || 
+    if(((hour ==  10 || hour == 22) && minute <= 22) ||
             ((hour ==  9 || hour == 21) && minute >= 23))
     {
         lightZehn2(cForeground);
     }
-    if(((hour ==  11 || hour == 23) && minute <= 22) || 
+    if(((hour ==  11 || hour == 23) && minute <= 22) ||
             ((hour ==  10 || hour == 22) && minute >= 23))
     {
         lightElf(cForeground);
     }
-    if(((hour ==  12 || hour == 0) && minute <= 22) || 
+    if(((hour ==  12 || hour == 0) && minute <= 22) ||
             ((hour ==  11 || hour == 23) && minute >= 23))
     {
         lightZwoelf(cForeground);
@@ -267,27 +189,27 @@ void checktime()
     {
         lightUhr(cForeground);
     }
-    if((hour ==  5  && minute >= 23) || 
+    if((hour ==  5  && minute >= 23) ||
             (hour >= 6 && hour <= 10) ||
             (hour ==  11  && minute <= 22))
     {
         lightVor2(cForeground);
         lightMittag(cForeground);
     }
-    if((hour ==  12  && minute >= 23) || 
+    if((hour ==  12  && minute >= 23) ||
             (hour >= 13 && hour <= 18) ||
             (hour ==  19  && minute <= 22))
     {
         lightNach2(cForeground);
         lightMittag(cForeground);
     }
-    if((hour ==  19  && minute >= 23) || 
+    if((hour ==  19  && minute >= 23) ||
             (hour >= 20 || hour <= 5) ||
             (hour ==  5  && minute <= 22))
     {
         lightNachts(cForeground);
     }
-    if((hour ==  11  && minute >= 23) || 
+    if((hour ==  11  && minute >= 23) ||
             (hour ==  12  && minute <= 22))
     {
         lightMittag(cForeground);
